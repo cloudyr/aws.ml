d06c8b86e5a864fcf25c1c5fa6d41e72789c5ff9
Thomas J. Leeper <thosjleeper@gmail.com>
2018-04-14 17:11:56 +0100

    cleanup http code & fix session_token arg

--- R/http.R
+++ R/http.R
@@ -2,11 +2,13 @@
 #' @description This is the workhorse function to execute calls to the Amazon ML API.
 #' @param action A character string specifying an API endpoint.
 #' @param query An optional named list containing query string parameters and their character values.
+#' @param headers A list of headers to pass to the HTTP request.
 #' @param body A list.
-#' @param region A character string containing an AWS region. If missing, the default \dQuote{us-east-1} is used.
-#' @param key A character string containing an AWS Access Key ID. See \code{\link[aws.signature]{locate_credentials}}.
-#' @param secret A character string containing an AWS Secret Access Key. See \code{\link[aws.signature]{locate_credentials}}.
-#' @param session_token Optionally, a character string containing an AWS temporary Session Token. See \code{\link[aws.signature]{locate_credentials}}.
+#' @param verbose A logical indicating whether to be verbose. Default is given by \code{options("verbose")}.
+#' @param region A character string specifying an AWS region. See \code{\link[aws.signature]{locate_credentials}}.
+#' @param key A character string specifying an AWS Access Key. See \code{\link[aws.signature]{locate_credentials}}.
+#' @param secret A character string specifying an AWS Secret Key. See \code{\link[aws.signature]{locate_credentials}}.
+#' @param session_token Optionally, a character string specifying an AWS temporary Session Token to use in signing a request. See \code{\link[aws.signature]{locate_credentials}}.
 #' @param \dots Additional arguments passed to \code{\link[httr]{GET}}.
 #' @return If successful, a named list. Otherwise, a data structure of class \dQuote{aws-error} containing any error message(s) from AWS and information about the request attempt.
 #' @details This function constructs and signs an Dynamo DB API request and returns the results thereof, or relevant debugging information in the case of error.
@@ -20,13 +22,23 @@ mlHTTP <-
 function(
   action,
   query = list(),
+  headers = list(),
   body = NULL,
-  region = Sys.getenv("AWS_DEFAULT_REGION","us-east-1"), 
+  verbose = getOption("verbose", FALSE),
+  region = Sys.getenv("AWS_DEFAULT_REGION", "us-east-1"), 
   key = NULL, 
   secret = NULL, 
   session_token = NULL,
   ...
 ) {
+    # locate and validate credentials
+    credentials <- locate_credentials(key = key, secret = secret, session_token = session_token, region = region, verbose = verbose)
+    key <- credentials[["key"]]
+    secret <- credentials[["secret"]]
+    session_token <- credentials[["session_token"]]
+    region <- credentials[["region"]]
+    
+    # generate request signature
     d_timestamp <- format(Sys.time(), "%Y%m%dT%H%M%SZ", tz = "UTC")
     url <- paste0("https://machinelearning.",region,".amazonaws.com")
     Sig <- signature_v4_auth(
@@ -43,17 +55,20 @@ function(
            request_body = if (length(body)) jsonlite::toJSON(body, auto_unbox = TRUE) else "",
            key = key,
            secret = secret,
-           session_token = session_token)
-    headers <- list(`x-amz-date` = d_timestamp,
-                    `x-amz-content-sha256` = Sig$BodyHash,
-                    `X-Amz-Target` = paste0("AmazonML_20141212.", action),
-                    "Content-Type" = "application/x-amz-json-1.1",
-                    Authorization = Sig$SignatureHeader)
+           session_token = session_token,
+           verbose = verbose)
+    # setup request headers
+    headers[["x-amz-date"]] <- d_timestamp
+    headers[["X-Amz-Target"]] <- paste0("AmazonML_20141212.", action)
+    headers[["x-amz-content-sha256"]] <- Sig$BodyHash
+    headers[["Content-Type"]] <- "application/x-amz-json-1.1"
+    headers[["Authorization"]] <- Sig[["SignatureHeader"]]
     if (!is.null(session_token) && session_token != "") {
         headers[["x-amz-security-token"]] <- session_token
     }
     H <- do.call(add_headers, headers)
     
+    # execute request
     r <- POST(url, H, body = body, encode = "json", ...)
     cont <- content(r, "text", encoding = "UTF-8")
     if (http_error(r)) {
